d0e2da7c633bddf9b64c5e2e6235185620b26cd5
stopmotionmanager <oreo4455@gmail.com>
2016-11-10 11:19:43 -0800

    Made code more readable, added comments for understandability

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
-
+// Main Loop
 task main()
 {
+	// Control Loop
 	while (1==1){
-		///Left, Right, Forward, Back controls
-		motor[port2] = vexRT[Ch1]-vexRT[Ch2]+vexRT[Ch4];
-		motor[port3] = -vexRT[Ch1]-vexRT[Ch2]+vexRT[Ch4];
-		motor[port4] = -vexRT[Ch1]+vexRT[Ch2]+vexRT[Ch4];
-		motor[port5] = vexRT[Ch1]+vexRT[Ch2]+vexRT[Ch4];
+		// Translates joystick control to motor actuation
+		motor[port2] =   vexRT[Ch1] - vexRT[Ch2] + vexRT[Ch4];
+		motor[port3] = - vexRT[Ch1] - vexRT[Ch2] + vexRT[Ch4];
+		motor[port4] = - vexRT[Ch1] + vexRT[Ch2] + vexRT[Ch4];
+		motor[port5] =   vexRT[Ch1] + vexRT[Ch2] + vexRT[Ch4];
 	}
 }
