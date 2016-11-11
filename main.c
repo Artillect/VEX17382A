39c04578e7487df56709ea36edfc29c5d917c622
stopmotionmanager <oreo4455@gmail.com>
2016-11-10 17:09:50 -0800

    Reduced speed of rotation and added button press to start main loop

--- main.c
+++ main.c
@@ -1,12 +1,19 @@
 // Main Loop
 task main()
 {
+	int start = 0;
+	// Makes the robot not do anything until button is pressed
+	while (start==0) {
+		if (vexRT[Btn8D] == 1) {
+			start = start + 1;
+		}
+	}
 	// Control Loop
-	while (1==1){
+	while (start == 1){
 		// Translates joystick control to motor actuation
-		motor[port2] =   vexRT[Ch1] - vexRT[Ch2] + vexRT[Ch4];
-		motor[port3] = - vexRT[Ch1] - vexRT[Ch2] + vexRT[Ch4];
-		motor[port4] = - vexRT[Ch1] + vexRT[Ch2] + vexRT[Ch4];
-		motor[port5] =   vexRT[Ch1] + vexRT[Ch2] + vexRT[Ch4];
+		motor[port2] =   vexRT[Ch1] - vexRT[Ch2] + vexRT[Ch4] * 0.75;
+		motor[port3] = - vexRT[Ch1] - vexRT[Ch2] + vexRT[Ch4] * 0.75;
+		motor[port4] = - vexRT[Ch1] + vexRT[Ch2] + vexRT[Ch4] * 0.75;
+		motor[port5] =   vexRT[Ch1] + vexRT[Ch2] + vexRT[Ch4] * 0.75;
 	}
 }
