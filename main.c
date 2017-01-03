772a1f49abfadb81014742382314d0bb8e906dda
stopmotionmanager <oreo4455@gmail.com>
2017-01-02 21:15:30 -0800

    Further Optimization
    I removed the stopmove and stoplift functions, and removed timing in all
    control functions. Stopmove and stoplift are essentially move and lift
    with 0 in all parameters. Timing isn't needed during user control and it
    is much more readable in autonomous to use the wait1Msec function.

--- main.c
+++ main.c
@@ -9,25 +9,11 @@
 
 // Controls robot movement.
 // Forward is how much forward, right is how much right, yawRate is how fast to turn, time is how long.
-void move(int forward, int right, int yawRate, float time) {
-	motor[port2] = - forward + right + yawRate * 0.75;
-	motor[port3] = forward + right + yawRate * 0.75;
-	motor[port4] = forward - right + yawRate * 0.75;
-	motor[port5] = -forward - right + yawRate * 0.75;
-	wait1Msec(time);
-}
-// Stops all of the robot's motors
-void stopMove() {
-	motor[port2] = 0;
-	motor[port3] = 0;
-	motor[port4] = 0;
-	motor[port5] = 0;
-}
-
-// Stops the lift
-void stopLift() {
-	motor[port6] = 0;
-	motor[port7] = 0;
+void move(int forward, int right, int yawRate) {
+	motor[port2] = - forward + right + yawRate;
+	motor[port3] = forward + right + yawRate;
+	motor[port4] = forward - right + yawRate;
+	motor[port5] = -forward - right + yawRate;
 }
 
 // Controls the lift
@@ -35,17 +21,9 @@ void stopLift() {
 // If direction is false -> move forklift down
 // Time controls how long this happens
 // TODO: Make this less hacky, there's got to be a better way of doing this
-void lift(bool direction, int power, int time) {
-	if (direction) {
+void lift(int power) {
 		motor[port6] = power;
 		motor[port7] = -power;
-		wait1Msec(time);
-	}
-	else {
-		motor[port6] = -power;
-		motor[port7] = power;
-		wait1Msec(time);
-	}
 }
 
 // This does stuff before the autonomous or user control periods start, don't set motor powers during this period.
@@ -61,16 +39,21 @@ void pre_auton()
 task autonomous()
 {
 		// Drop forklift
-		move(0, -127, 0, 0);
-		lift(true, 63, 250);
-		lift(false, 127, 0);
-		move(0, 127, 0, 250);
-		lift(true, 127, 750);
-		move(0, 127, 0, 1900);
-		stopMove();
-		stopLift();
-		lift(true, 127, 250);
-		stopLift();
+		move(0, -127, 0);
+		lift(63);
+		wait1Msec(250);
+		lift(-127);
+		move(0, 127, 0);
+		wait1Msec(250);
+		lift(127);
+		wait1Msec(750);
+		move(0, 127, 0);
+		wait1Msec(1900);
+		move(0, 0, 0);
+		lift(0);
+		lift(127);
+		wait1Msec(250);
+		lift(0);
 }
 
 // This controls the robot during the user control period of the competition.
@@ -80,15 +63,15 @@ task usercontrol()
   while (true)
   {
 		// Translates joystick control to movement
-		move(vexRT[Ch1], vexRT[Ch2], vexRT[Ch4], 0);
+		move(vexRT[Ch1], vexRT[Ch2], vexRT[Ch4] * 0.75);
 
 		// Controls the lift
 		// TODO: Make less hacky, works but I don't like it.
 		if (vexRT[Btn6U]) {
-			lift(true, 127, 0);
+			lift(127);
 		}
 		if (vexRT[Btn5U]) {
-			lift(false, 127, 0);
+			lift(-127);
 		}
   }
 }
