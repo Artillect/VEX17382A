d7501a352de519ae20f475ca9e3e02d183d177ff
stopmotionmanager <oreo4455@gmail.com>
2017-01-16 09:42:36 -0800

    Changes

--- main.c
+++ main.c
@@ -10,16 +10,20 @@
 // Controls robot movement.
 // Forward is how much forward, right is how much right, yawPower is how much power to turn with
 void move(int forward, int right, int yawPower) {
-	motor[port2] = - forward + right + yawPower;
-	motor[port3] = forward + right + yawPower;
-	motor[port4] = forward - right + yawPower;
-	motor[port5] = -forward - right + yawPower;
+	motor[port4] = - forward + right + yawPower;
+	motor[port5] = forward + right + yawPower;
+	motor[port6] = forward - right + yawPower;
+	motor[port7] = -forward - right + yawPower;
 }
 
 // Controls the lift
 void lift(int power) {
-		motor[port6] = power;
-		motor[port7] = -power;
+		motor[port1] = power; //Yes this is correct, don't touch it, it works just fine
+		motor[port2] = -power;
+		motor[port3] = -power;
+		motor[port8] = power;
+		motor[port9] = power;
+		motor[port10] = power;
 }
 
 // This does stuff before the autonomous or user control periods start, don't set motor powers during this period.
@@ -34,19 +38,15 @@ void pre_auton()
 task autonomous()
 {
 		// Drop forklift from locked position
-		move(-127, 0, 0);
+		move(0, -127, 0);
 		lift(63);
 		wait1Msec(250);
 		lift(-127);
-		move(127, 0, 0);
+		move(0, 127, 0);
 		wait1Msec(250);
 
-		// Lift lift
-		lift(127);
-		wait1Msec(750);
-
 		// Move right for a bit
-		move(0, 127, 0);
+		move(127, 0, 0);
 		wait1Msec(1900);
 
 		// Stop Moving
@@ -55,32 +55,34 @@ task autonomous()
 		wait1Msec(200);
 
 		// Lift lift all the way
-		lift(127);
-		wait1Msec(250);
+		lift(100);
+		wait1Msec(1500);
 
 		// Stop lift
 		lift(0);
 }
 
 // This controls the robot during the user control period of the competition.
-task usercontrol()
-{
-  while (true)
-  {
-		// Translates joystick control to movement
+task usercontrol() {
+
+  while (true) {
+		// Calls move with vertical movement controlled by Ch1, horizontal movement controlled by Ch2, and rotation controlled by Ch4 (scaled my 0.75 because it feels right)
 		move(vexRT[Ch1], vexRT[Ch2], vexRT[Ch4] * 0.75);
 
 		// Controls the lift
 		// Is a bit more streamlined than the next example, harder to read though
-		lift(127 * vexRT[Btn6U] - 127 * vexRT[Btn6U]);
+		//lift(127 * vexRT[Btn6U] - 127 * vexRT[Btn5U]);
 
 		// I think this works alongside the rest of the code, it does have an unnecessary if check though...
 		// This snippet of code is equivalent to the previous uncommented line.
-		//if vexRT[Btn6U]) {
-		//	lift(127);
-		//}
-		//if (vexRT[Btn5U]) {
-		//	lift(-127);
-		//}
+		if (vexRT[Btn6U]) {
+			lift(127);
+		}
+		else if (vexRT[Btn5U]) {
+			lift(-127);
+		}
+		else {
+			lift(0);
+		}
   }
 }
