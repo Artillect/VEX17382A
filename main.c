74b255cd9d90bc2adf738bcfbce60cd644003914
stopmotionmanager <oreo4455@gmail.com>
2017-03-08 18:03:13 -0800

    Began Work on Skills and auton

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#pragma config(Sensor, dgtl1,  solenoidRight,    sensorDigitalOut)
+ #pragma config(Sensor, dgtl1,  solenoidRight,    sensorDigitalOut)
 #pragma config(Sensor, dgtl2,  solenoidLeft,     sensorDigitalOut)
 
 // This code is for the VEX cortex platform
@@ -42,58 +42,6 @@ void pre_auton() {
 
 // This code controls the robot during the autonomous period of the competition.
 task autonomous() {
-	// robot is starting from the right, use bottom code when from the left
-	// Drop forklift from locked position
-	move(-127, 0);
-	lift(63);
-	wait1Msec(250);
-	lift(-127);
-	move(127, 0);
-	wait1Msec(250);
-
-	// Start movin
-	pneumaticClaw(true);
-	move(127, 0);
-	lift(127);
-	wait1Msec(1500);
-	pneumaticClaw(false);
-	wait1Msec(250);
-	move(-127, 0);
-	wait1Msec(250);
-	lift(-127);
-	wait1Msec(500);
-
-	// Flip some stars (switch yaw signs around if starting on left)
-	move(0, -127);
-	wait1Msec(750);
-	move(-127, 0);
-	wait1Msec(250);
-	move(0, -127);
-	wait1Msec(250);
-	lift(127);
-	wait1Msec(1000);
-	lift(-127);
-	wait1Msec(500);
-	move(0, 127);
-	wait1Msec(2000);
-	lift(127);
-	wait1Msec(1000);
-	lift(-127);
-	wait1Msec(1000);
-
-	// INCOMPLETE - Gets 3 farzone stars, either dump or get to favorable spot
-	move(63, -63);
-	wait1Msec(250);
-	move(0, -127);
-	wait1Msec(100);
-	move(127, 0);
-	wait1Msec(500);
-	wait1Msec(250);
-	move(63, 63);
-	wait1Msec(500);
-	pneumaticClaw(false);
-	move(0,127);
-	wait1Msec(500);
 
 
 
@@ -154,7 +102,107 @@ task autonomous() {
 */
 // This controls the robot during the user control period of the competition.
 task usercontrol() {
+	// robot is starting from the right, use bottom code when from the left
+	// Drop forklift from locked position
+	move(-127, 0);
+	lift(63);
+	wait1Msec(250);
+	lift(-127);
+	move(127, 0);
+	wait1Msec(250);
+	lift(0);
 
+	// Move back and then forward to grab stars
+	move(-127,0);
+	wait1Msec(500); // Change depending on amount of time it takes for forklift to get off of the stars
+	pneumaticClaw(true);
+	move(127,0);
+	wait1Msec(1500); // Change depending on amount of time it takes to scoop up the stars in front of it.
+	pneumaticClaw(false);
+	move(0,127);
+	wait1Msec(1400); // Change depending on amount of time it takes to rotate 90 degrees
+	/* Uncomment this if you want to lift the stars before moving
+	lift(63);
+	wait1Msec(250);
+	lift(10);
+	*/
+
+	// Move forward with grabbed stars
+	move(127,0);
+	wait1Msec(3500);
+	move(0,127);
+	wait1Msec(2800);
+
+	// Dump stars
+	move(-127,0);
+	lift(127);
+	wait1Msec(500);
+	pneumaticClaw(true);
+	lift(0);
+	move(0,127);
+	wait1Msec(1400);
+
+	move(-127,0);
+	wait1Msec(2000);
+
+	move(0,-127);
+	wait1Msec(1400);
+
+	move(127,0);
+	wait1Msec(3500);
+	pneumaticClaw(false);
+	move(-127,0);
+	wait1Msec(3500);
+	move(-127,0);
+	lift(127);
+	wait1Msec(500);
+	pneumaticClaw(true);
+	lift(0);
+	/*
+	// Start movin
+	pneumaticClaw(true);
+	move(127, 0);
+	lift(127);
+	wait1Msec(1500);
+	pneumaticClaw(false);
+	wait1Msec(250);
+	move(-127, 0);
+	wait1Msec(250);
+	lift(-127);
+	wait1Msec(500);
+
+	// Flip some stars (switch yaw signs around if starting on left)
+	move(0, -127);
+	wait1Msec(750);
+	move(-127, 0);
+	wait1Msec(250);
+	move(0, -127);
+	wait1Msec(250);
+	lift(127);
+	wait1Msec(1000);
+	lift(-127);
+	wait1Msec(500);
+	move(0, 127);
+	wait1Msec(2000);
+	lift(127);
+	wait1Msec(1000);
+	lift(-127);
+	wait1Msec(1000);
+
+	// INCOMPLETE - Gets 3 farzone stars, either dump or get to favorable spot
+	move(63, -63);
+	wait1Msec(250);
+	move(0, -127);
+	wait1Msec(100);
+	move(127, 0);
+	wait1Msec(500);
+	wait1Msec(250);
+	move(63, 63);
+	wait1Msec(500);
+	pneumaticClaw(false);
+	move(0,127);
+	wait1Msec(500);
+ 	*/
   while (true) {
 		// Calls move with vertical movement controlled by Ch1, horizontal movement controlled by Ch2, and rotation controlled by Ch4 (scaled my 0.75 because it feels right)
 		move(vexRT[Ch2], vexRT[Ch4] * 0.75);
